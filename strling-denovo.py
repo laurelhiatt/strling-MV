b23132f65ec755394a5a6987683eafbf01c76a22
Laurel (they/he) <laurel.hiatt@gmail.com>
2021-09-27 17:49:42 -0600

    fixed a couple things

--- strling-denovo.py
+++ strling-denovo.py
@@ -5,27 +5,29 @@ def get_args():
     """Incorporating argparse into the code for interchangeable arguments"""
     parser = argparse.ArgumentParser()
     parser.add_argument("--outliers", required=True,
-    ### strling output goes here as input
+    # strling output goes here as input
         help="input outlier name")
     parser.add_argument("--ped", required=True,
         help="input ped file")
-    ### ped file to sort trios
+    # ped file to sort trios
     parser.add_argument("--out",
         help="outputfile")
-    ### out will just be the name of my output file... turn up
+    # out will just be the name of the output file... turn up
     parser.add_argument("--wiggle", default=0.1,
-        help="establishes the ranges we are setting for the alleles (default: %(default)s)")
-    ### cannot be greater than 1
+        help="establishes the ranges we are setting for the alleles (default:%(default)s)")
+    # value between 0 and 1
     parser.add_argument("--minwig", default=10.0,
-        help="minimum wiggle for small alleles")
+        help="minimum wiggle for small alleles (default: %(default)s)")
     parser.add_argument("--depth", type=int, default=15,
-        help="depth filter")
+        help="depth filter (default: %(default)s)")
     parser.add_argument("--ampsize", type=int, default=150,
-        help="amplification size filter")
-        ### size of de novo expansion, or difference from kid to mom and dad allele sizes, is defaulted to 150bp
+        help="amplification size filter (default: %(default)s)")
+        # size of de novo expansion, or difference from kid to mom/dad allele sizes
+        #defaulted to 150bp based on elbow plots of Lynch data set
     return parser.parse_args()
 
 def main():
+    """Culminating function to establish amplifications and Mendelian status"""
     args = get_args()
     denovo.get_denovos(args)
 
